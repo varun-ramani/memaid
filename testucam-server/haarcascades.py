392f15ed8b3e4e227bf3b19db35268887bd8ccd0
Vibhu Agrawal <vibhu1.agrawal@gmail.com>
2022-04-10 07:00:12 -0400

    Added error fixing

--- testucam-server/haarcascades.py
+++ testucam-server/haarcascades.py
@@ -9,5 +9,9 @@ faces = faceCascade.detectMultiScale(
     minSize=(30, 30),
     flags = cv2.cv.CV_HAAR_SCALE_IMAGE
 )
+print ("Found {0} faces!".format(len(faces)))
+# Draw a rectangle around the faces
+for (x, y, w, h) in faces:
+    cv2.rectangle(image, (x, y), (x+w, y+h), (0, 255, 0), 2)
 cv2.imshow("Faces found", img)
 cv2.waitKey(0)
