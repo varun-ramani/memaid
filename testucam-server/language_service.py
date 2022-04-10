0401d79afd1a6e492933d3d590ad644814692c88
Varun Ramani <varun.ramani@gmail.com>
2022-04-10 06:39:19 -0400

    Made basic arcitechture, added nlp to language service Co-authored by: @VibhuAg <vibhu1.agrawal@gmail.com>

---
+++ testucam-server/language_service.py
@@ -0,0 +1,88 @@
+from queue import Queue
+from threading import Thread
+from http.client import responses
+import requests as re
+import os
+from google.cloud import language_v1
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = 'service-account-file.json'
+
+transcription_queue = Queue()
+
+def sample_analyze_entities(text_content):
+    bullet_points = []
+    """
+    Analyzing Entities in a String
+
+    Args:
+      text_content The text content to analyze
+    """
+
+    client = language_v1.LanguageServiceClient()
+
+    # text_content = 'California is a state.'
+
+    # Available types: PLAIN_TEXT, HTML
+    type_ = language_v1.Document.Type.PLAIN_TEXT
+
+    # Optional. If not specified, the language is automatically detected.
+    # For list of supported languages:
+    # https://cloud.google.com/natural-language/docs/languages
+    language = "en"
+    document = {"content": text_content, "type_": type_, "language": language}
+
+    # Available values: NONE, UTF8, UTF16, UTF32
+    encoding_type = language_v1.EncodingType.UTF8
+
+    response = client.analyze_entities(request = {'document': document, 'encoding_type': encoding_type})
+    persons = []
+    # Loop through entitites returned from the API
+    for entity in response.entities:
+        print(u"Representative name for the entity: {}".format(entity.name))
+        entity_type = language_v1.Entity.Type(entity.type_).name
+        # Get entity type, e.g. PERSON, LOCATION, ADDRESS, NUMBER, et al
+        print(u"Entity type: {}".format(language_v1.Entity.Type(entity.type_).name))
+        if entity_type == "PERSON":
+            persons.append(entity.name)
+        # Get the salience score associated with the entity in the [0, 1.0] range
+        print(u"Salience score: {}".format(entity.salience))
+        if entity.salience > 0.05:
+            bullet_points.append(entity.name)
+
+    
+    return persons, bullet_points
+
+def add_transcription(sentence):
+    transcription_queue.put(sentence)
+    
+my_name = "Mark"
+def start_language_service():
+    global my_name
+    while True:
+        if not transcription_queue.empty():
+            next_transcription = transcription_queue.get()
+            # check if we're looking at something
+            print(next_transcription)
+            persons, bullet_points = sample_analyze_entities(next_transcription)
+            looking_for = "my name is"
+            punctuation = "!\"'(),.:;?`"
+            name_start = text_content.lower().find(looking_for)
+            if name_start != -1:
+                name_rest = text_content[name_start + len(looking_for) + 1:]
+                name = name_rest[:name_rest.find(" ")]
+                name = name.strip()
+                for character in punctuation:
+                    name = name.replace(character, '')
+                if name.lower() != my_name.lower():
+                    print(name)
+            elif text_content.lower().find("i'm") != -1:
+                name_start = text_content.lower().find("i'm")
+                name_rest = text_content[name_start + len("i'm") + 1:]
+                name = name_rest[:name_rest.find(" ")]
+                for character in punctuation:
+                    name = name.replace(character, '')
+                
+                if name in persons and name.lower() != my_name.lower():
+                    print(name)
+
+language_service_thread = Thread(target=start_language_service)
+language_service_thread.start()
